8ee314bd7cbc85f2fd45c0571a6f1cf75e2a1c1a
milan <milan.chule@gmail.com>
2018-09-20 23:56:10 +0200

    changed UI

--- ProductApp.Infrastructure/CategoryInitializeDB.cs
+++ ProductApp.Infrastructure/CategoryInitializeDB.cs
@@ -1,9 +1,10 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using ProductApp.Core;
 
 namespace ProductApp.Infrastructure
 {
-    public class CategoryInitializeDB : DropCreateDatabaseIfModelChanges<ProductContext>    //ovde moze DropCreateDatabaseAlways, ali ce obrisati i sve proizvode u bazi i indekse
+    public class CategoryInitializeDB : DropCreateDatabaseAlways<ProductContext> 
     {
         protected override void Seed(ProductContext context) 
         {
@@ -25,15 +26,35 @@ namespace ProductApp.Infrastructure
                 Name = "Other"
             });
 
-            //context.Product.Add(new Product
-            //{
-            //    Id = 1,
-            //    Name = "Orange",
-            //    Price = 50,
-            //    CategoryId = 1,
-            //    IsActive = true,
-            //    Created = DateTime.Now
-            //});
+            context.Product.Add(new Product
+            {
+                Id = 1,
+                Name = "Banana",
+                Price = 70,
+                CategoryId = 1,
+                IsActive = true,
+                Created = DateTime.Now
+            });
+
+            context.Product.Add(new Product
+            {
+                Id = 2,
+                Name = "Olive oil",
+                Price = 97,
+                CategoryId = 3,
+                IsActive = false,
+                Modified = DateTime.Now
+            });
+
+            context.Product.Add(new Product
+            {
+                Id = 3,
+                Name = "Orange juice",
+                Price = 45,
+                CategoryId = 2,
+                IsActive = true,
+                Created = DateTime.Now
+            });
 
             context.SaveChanges();
             base.Seed(context);
